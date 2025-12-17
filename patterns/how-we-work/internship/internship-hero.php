d43c72d33427f0838d1baa1d7dafe53ac6f8ce7f
Kai Kameyama <1203kk06@gmail.com>
2025-12-17 20:47:59 +0800

    Internship Process

--- patterns/how-we-work/internship/internship-hero.php
+++ patterns/how-we-work/internship/internship-hero.php
@@ -1,53 +1,43 @@
 <?php
+
 /**
  * Title: Internship Hero Section
  * Slug: svlti/internship-hero
  */
 ?>
 
-<!-- wp:group {"className":"relative max-w-7xl min-h-[90vh]","layout":{"type":"flex"}} -->
-<div class="wp-block-group relative flex max-w-7xl min-h-[90vh] w-full">
+<!-- wp:group {"className":"relative max-w-7xl min-h-[90vh]","layout":{"type":"flex","flexWrap":"wrap"}} -->
+<div class="wp-block-group relative flex flex-wrap min-h-[90vh] w-full">
 
-<!-- wp:group {"className":"flex flex-row","layout":{"type":"constrained"}} -->
-<div class="wp-block-group flex flex-row"> 
+    <!-- wp:group {"className":"items-start justify-center h-[75vh] bg-light-blue","layout":{"type":"flex","orientation":"horizontal"}} -->
+    <div class="wp-block-group flex flex-col items-end justify-center h-[75vh]  bg-[#7EDDC3]/60 flex-1">
 
-  <!-- wp:group {"className":"h-[65vh] w-[20%]",{"type":"constrained"}} -->
-  <div class="wp-block-group h-[65vh] w-[20%]">
-  </div>
-  <!-- /wp:group -->
+        <!-- wp:group {"className":"md:w-[52%]","layout":{"type":"flex"}} -->
+        <div class="wp-block-group flex-col items-end p-12 max-w-3xl">
 
-  <!-- wp:group {"className":"items-end justify-center h-[65vh] bg-[#7EDDC3]/60","layout":{"type":"flex","orientation":"vertical"}} -->
-  <div class="wp-block-group flex flex-col items-end justify-center h-[65vh] bg-[#7EDDC3]/60">
+            <!-- wp:heading {"level":2,"align":"left","className":"text-[56px] font-semibold"} -->
+            <h2 class="wp-block-heading text-[34px] 2xl:text-[56px] font-semibold">
+                <span class="text-dark-green drop-shadow">Internship</span> <span class="text-dark-green drop-shadow">Process</span>
+            </h2>
+            <!-- /wp:heading -->
 
-    <!-- wp:group {"className":"items-start p-12 md:w-[52%] assessment-container","layout":{"type":"flex","orientation":"vertical"}} -->
-    <div class="wp-block-group flex-col items-start p-12 md:w-[55%]"> 
+            <!-- wp:paragraph {"className":"text-gray-800 leading-relaxed"} -->
+            <p class="text-gray-800 font-medium text-[18px] 2xl:text-[30px] leading-relaxed text-right">
+            Internships give students a chance to apply what they learn in class to real work settings.
+            Our process is designed to support students, families, and partner organizations in creating a meaningful experience for growth and career readiness.
+            </p>
+            <!-- /wp:paragraph -->
 
-      <!-- wp:heading {"level":2,"align":"right","className":"text-[56px] font-semibold"} -->
-      <h2 class="wp-block-heading text-[56px] font-semibold has-text-align-right">
-        <span class="text-dark-green drop-shadow">Internship</span> <span class="text-dark-green drop-shadow">Process</span>
-      </h2>
-      <!-- /wp:heading -->
-    
-      <!-- wp:paragraph {"align":"right","className":"text-gray-800 leading-relaxed"} -->
-      <p class="text-gray-800 font-medium leading-relaxed has-text-align-right">
-        Internships give students a chance to apply what they learn in class to real work settings. 
-        Our process is designed to support students, families, and partner organizations in creating a meaningful experience for growth and career readiness.
-      </p>
-      <!-- /wp:paragraph -->
-
+        </div>
+        <!-- /wp:group -->
     </div>
     <!-- /wp:group -->
-  </div>
-  <!-- /wp:group -->
-  
-  </div>
-  <!-- /wp:group -->
 
-  <!-- wp:image {"sizeSlug":"large","className":"shadow-lg z-10"} -->
-  <figure class="wp-block-image size-large absolute top-2 left-4 w-[28rem] shadow-lg z-10">
-    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/how-we-work/internship.png" alt="Children in classroom" />
-  </figure>
-  <!-- /wp:image -->
+    <!-- wp:image {"sizeSlug":"large","className":"shadow-lg z-10"} -->
+    <figure class="wp-block-image size-large absolute -top-4 left-4 w-[10rem] lg:w-[18rem] xl:w-[22rem] 2xl:w-[38rem] shadow-lg z-10">
+        <img class="w-full h-auto object-cover" src="<?php echo get_template_directory_uri(); ?>/assets/images/how-we-work/internship.png" alt="Children in classroom" />
+    </figure>
+    <!-- /wp:image -->
 
 </div>
 <!-- /wp:group -->
