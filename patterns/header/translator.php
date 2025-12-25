743d041fc1bfc240ab6f826fdf8bc35fd4713f08
Jun Wei <156562637+junwei07@users.noreply.github.com>
2025-12-25 22:50:49 +0800

    Update header and drop-down menus

---
+++ patterns/header/translator.php
@@ -0,0 +1,10 @@
+<?php 
+/**
+ * Title: Translate Button
+ * Slug: svlti/translate-button
+ */
+?>
+
+<!-- wp:shortcode -->
+<?php echo do_shortcode('[language-switcher]'); ?>
+<!-- /wp:shortcode -->
