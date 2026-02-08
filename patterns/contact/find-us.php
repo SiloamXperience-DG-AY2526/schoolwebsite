247bb8bc5be9e2daa63048061331af9aa6a21b92
Jun Wei <156562637+junwei07@users.noreply.github.com>
2026-02-08 18:31:37 +0800

    Update find us location

--- patterns/contact/find-us.php
+++ patterns/contact/find-us.php
@@ -27,8 +27,8 @@
                     <h2 class="wp-block-heading text-header-xxl text-eucalyptus-100 mb-6">Find Us</h2>
                     <!-- /wp:heading -->
 
-                    <!-- wp:group {"className":"w-full bg-white rounded-lg shadow-lg overflow-hidden","layout":{"type":"constrained"}} -->
-                    <div class="wp-block-group w-full bg-white rounded-lg shadow-lg overflow-hidden">
+                    <!-- wp:group {"className":"w-full bg-white rounded-lg shadow-lg overflow-hidden mb-6","layout":{"type":"constrained"}} -->
+                    <div class="wp-block-group w-full bg-white rounded-lg shadow-lg overflow-hidden m-6">
 
                         <!-- wp:group {"className":"","layout":{"type":"constrained"}} -->
                         <div class="wp-block-group">
@@ -77,6 +77,64 @@
                                     <!-- wp:paragraph {"className":"text-gray-600 text-sm"} -->
                                     <p class="text-gray-600 text-sm">
                                         (Opposite Tuol Damnak Secondary School)
+                                    </p>
+                                    <!-- /wp:paragraph -->
+                                </div>
+                                <!-- /wp:group -->
+
+                            </div>
+                            <!-- /wp:group -->
+
+                        </div>
+                        <!-- /wp:group -->
+
+                    </div>
+                    <!-- /wp:group -->
+
+                    <!-- wp:group {"className":"w-full bg-white rounded-lg shadow-lg overflow-hidden","layout":{"type":"constrained"}} -->
+                    <div class="wp-block-group w-full bg-white rounded-lg shadow-lg overflow-hidden">
+
+                        <!-- wp:group {"className":"","layout":{"type":"constrained"}} -->
+                        <div class="wp-block-group">
+                            <!-- wp:image {"alt":"Campus Building","className":"w-full h-auto rounded-lg object-cover"} -->
+                            <figure class="wp-block-image w-full h-auto rounded-lg object-cover find-img">
+                                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/places/chong-doung-commune-hall.jpg" alt="Campus Building" />
+                            </figure>
+                            <!-- /wp:image -->
+                        </div>
+                        <!-- /wp:group -->
+
+                        <!-- wp:group {"className":"p-8","layout":{"type":"constrained"}} -->
+                        <div class="wp-block-group p-8">
+
+                            <!-- wp:group {"className":"flex flex-col gap-4 ml-0","layout":{"type":"default"}} -->
+                            <div class="wp-block-group flex flex-col gap-4 ml-0">
+
+                                <!-- wp:group {"className":"flex items-center gap-3","layout":{"type":"default"}} -->
+                                <div class="wp-block-group flex items-center gap-3">
+                                    <!-- wp:image {"sizeSlug":"thumbnail","className":"w-[24px]"} -->
+                                    <figure class="wp-block-image size-thumbnail w-[24px]">
+                                        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/abstract/pin.png" alt="pin" />
+                                    </figure>
+                                    <!-- /wp:image -->
+
+                                    <!-- wp:heading {"level":2,"className":"text-2xl text-header-sm text-elm-120"} -->
+                                    <h2 class="wp-block-heading text-2xl text-header-sm text-elm-120">
+                                        <a href="https://maps.app.goo.gl/vyR8Fdqc3PVLDcgg7?g_st=iw" target="_blank" rel="noopener noreferrer"
+                                            class="underline hover:no-underline">
+                                            Address
+                                        </a>
+                                    </h2>
+                                    <!-- /wp:heading -->
+
+                                </div>
+                                <!-- /wp:group -->
+
+                                <!-- wp:group {"layout":{"type":"constrained"}} -->
+                                <div class="wp-block-group">
+                                    <!-- wp:paragraph {"className":"text-gray-700 leading-relaxed mb-1"} -->
+                                    <p class="text-gray-700 leading-relaxed mb-1">
+                                        Chong Doung Commune Hall      
                                     </p>
                                     <!-- /wp:paragraph -->
                                 </div>
